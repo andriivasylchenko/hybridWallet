b1b64d7413d59dc38f6d10843902a92402f70a6a
andriivasylchenko <vasylchenko.andrii@gmail.com>
2015-10-20 05:05:36 -0400

    lab 2.46 results

---
+++ hybridWalletCordova/platforms/android/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":2307637610,"date":1445331577590,"machine":"ibm-virtual-machine"}
+/* Date: Tue Oct 20 2015 04:59:37 GMT-0400 (EDT) */
---
+++ hybridWalletServer/adapters/walletAdapter/walletAdapter-impl.js
@@ -0,0 +1,47 @@
+function getCards() {
+
+	var input = {
+	    method : 'get',
+	    returnedContentType : 'json',
+	    path : '/getCards'
+	};
+
+
+	return WL.Server.invokeHttp(input);
+}
+
+function getLimit() {
+
+	var input = {
+	    method : 'get',
+	    returnedContentType : 'json',
+	    path : '/getLimit'
+	};
+
+
+	return WL.Server.invokeHttp(input);
+}
+
+function getCardTransactions(cardId) {
+
+	var input = {
+	    method : 'get',
+	    returnedContentType : 'json',
+	    path : '/getCardTransactions/' + cardId
+	};
+
+
+	return WL.Server.invokeHttp(input);
+}
+
+function setLimit(limit) {
+
+	var input = {
+	    method : 'get',
+	    returnedContentType : 'json',
+	    path : '/setLimit'
+	};
+
+
+	return WL.Server.invokeHttp(input);
+}
